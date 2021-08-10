b11325670bb43845e97d9b774d71c1e07dc042cf
fox <zhouxiaocongmail@foxmail.com>
2021-08-10 10:44:36 +0800

    remove dist

--- dist/autoWidthInput/index.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { AutoWidthInputInterface } from './type';
-import { useInputFocuse } from './hooks';
-declare const AutoWidthInput: React.FC<AutoWidthInputInterface>;
-export { AutoWidthInput, useInputFocuse };
